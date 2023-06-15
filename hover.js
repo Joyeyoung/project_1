4df87f8288d2ee82eafa7d8b7a2745b839770a13
Joyeyoung <joyeyojng123@gmail.com>
2023-06-15 19:07:03 +0900

    123

---
+++ hover.js
@@ -0,0 +1,28 @@
+
+let best = document.getElementById('best1');
+function mouseover (){
+    best.setAttribute("src","./img/tit_content_1.gif");
+}
+
+function mouseleave (){
+    best.setAttribute("src","./img/tit_content_1.png");
+}       
+
+
+let best2 = document.getElementById('best2');
+function mouseover2 (){
+    best2.setAttribute("src","./img/tit_content_2.gif");
+}
+
+function mouseleave2 (){
+    best2.setAttribute("src","./img/tit_content_2.png");
+}   
+
+let best3 = document.getElementById('best3');
+function mouseover3 (){
+    best3.setAttribute("src","./img/tit_content_3.gif");
+}
+
+function mouseleave3 (){
+    best3.setAttribute("src",".");
+}       
